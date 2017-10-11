585a46bf8e536e048cda45e254fa7de7729927e6
jonmsawyer <jonmsawyer@gmail.com>
2017-10-10 17:18:22 -0800

    Initial

---
+++ apps/jsonbench/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JsonbenchConfig(AppConfig):
+    name = 'jsonbench'
---
+++ apps/m2mbench/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class M2MbenchConfig(AppConfig):
+    name = 'm2mbench'
