ae31ae6ffdc27bd6164d050e4df6ab4ad1e52895
Jonathan Sawyer <jonmsawyer@gmail.com>
2017-10-11 17:52:45 -0800

    Removing status username 'thales' from scripts in favor of a dynamic username initialization.
    Added requirements.txt file for the project.

--- scripts/read_posts.py
+++ scripts/read_posts.py
@@ -13,8 +13,28 @@ from apps.jsonbench.models import Post as jPost, ForumUser
 from apps.m2mbench.models import Post as mPost
 
 
-jsonbench_user = ForumUser.objects.get(user__username='thales')
-m2mbench_user = User.objects.get(username='thales')
+try:
+    username = str(sys.argv[1]).strip()
+except Exception as e:
+    os_user = os.environ.get('USERNAME') or os.environ.get('USER', '')
+    try:
+        username = input('Please enter a username to read posts as ({}): '.format(os_user))
+        if username.strip() is '':
+            username = os_user
+        if username is None or username is '':
+            raise Exception('invalid username: {}'.format(username))
+    except Exception as e2:
+        print('Error! could not obtain proper username: {}'.format(e2))
+        print('Usage: read_posts.py {username}')
+        sys.exit(1)
+
+try:
+    jsonbench_user = ForumUser.objects.get(user__username=username)
+    m2mbench_user = User.objects.get(username=username)
+except Exception as e:
+    print('Error! argument required: username')
+    print('Usage: read_posts.py {username}')
+    sys.exit(1)
 
 print('deleting read jsonbench posts ... ', end='', flush=True)
 jsonbench_user.posts_read = None
@@ -22,7 +42,7 @@ jsonbench_user.save()
 print('success!')
 
 count = 0
-m2mbench_read_posts = mPost.objects.filter(readers__username='thales')
+m2mbench_read_posts = mPost.objects.filter(readers__username=username)
 print('deleting {} m2mbench posts ...'.format(m2mbench_read_posts.count()), end='', flush=True)
 for m2mbench_read_post in m2mbench_read_posts:
     count += 1
@@ -93,7 +113,7 @@ for i in range(0, len(jsonbench_ids), chunk_range):
 print(' ... success!')
 
 print('reading posts for jsonbench ... ', end='', flush=True)
-jsonbench_user = ForumUser.objects.get(user__username='thales')
+jsonbench_user = ForumUser.objects.get(user__username=username)
 jsonbench_user.posts_read = json.dumps(jdict, indent=4)
 jsonbench_user.save()
 print(' ... success!')
