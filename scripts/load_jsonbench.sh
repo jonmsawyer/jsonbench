618f47af0ba841b33560121ee87439770f07d48f
Jonathan Sawyer <jonmsawyer@gmail.com>
2017-10-12 02:00:18 +0000

    Adding *nix scripts

---
+++ scripts/load_jsonbench.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+../manage.py loaddata jsonbench.board01.json
+../manage.py loaddata jsonbench.board02.json
+../manage.py loaddata jsonbench.board03.json
+../manage.py loaddata jsonbench.board04.json
+../manage.py loaddata jsonbench.board05.json
+../manage.py loaddata jsonbench.board06.json
+../manage.py loaddata jsonbench.board07.json
+../manage.py loaddata jsonbench.board08.json
+../manage.py loaddata jsonbench.board09.json
+../manage.py loaddata jsonbench.board10.json
