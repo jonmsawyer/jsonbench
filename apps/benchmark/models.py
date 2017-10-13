ae59a32764d0fa96cd0c5cac3fb6ed174908eb30
Jonathan Sawyer <jonmsawyer@gmail.com>
2017-10-13 13:23:25 -0800

    Updated Benchmark model

--- apps/benchmark/models.py
+++ apps/benchmark/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 
-class Benchmark(models.Model):
-    app = models.CharField(max_length=255, blank=True, null=True)
-    suite = models.CharField(max_length=255, blank=True, null=True)
-    suite_description = models.CharField(max_length=255, blank=True, null=True)
+class BenchmarkSuite(models.Model):
+    suite_name = models.CharField(max_length=255, blank=True, null=True)
+    suite_docstring = models.CharField(max_length=1000, blank=True, null=True)
     command_line_arguments = models.CharField(max_length=255, blank=True, null=True)
+    app_to_benchmark = models.CharField(max_length=255, blank=True, null=True)
     db_type = models.CharField(max_length=255, blank=True, null=True)
     db_info = models.CharField(max_length=255, blank=True, null=True)
-    db_stat_before_benchmark = models.CharField(max_length=255, blank=True, null=True)
-    db_stat_after_benchmark = models.CharField(max_length=255, blank=True, null=True)
-    computer_info = models.CharField(max_length=255, blank=True, null=True)
-    django_version = models.CharField(max_length=255, blank=True, null=True)
+    db_stat_before_suite = models.CharField(max_length=255, blank=True, null=True)
+    db_stat_after_suite = models.CharField(max_length=255, blank=True, null=True)
+    cpu_info = models.CharField(max_length=255, blank=True, null=True)
+    python_info = models.CharField(max_length=255, blank=True, null=True)
+    django_info = models.CharField(max_length=255, blank=True, null=True)
     django_user = models.CharField(max_length=255, blank=True, null=True)
     last_handled_exception = models.CharField(max_length=255, blank=True, null=True)
     last_unhandled_exception = models.CharField(max_length=255, blank=True, null=True)
@@ -21,10 +22,11 @@ class Benchmark(models.Model):
     end_time = models.DateTimeField(blank=True, null=True)
     duration = models.DurationField(blank=True, null=True)
     is_complete = models.NullBooleanField()
-    created_at = models.DateTimeField(auto_now=True, blank=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True, blank=True, null=True)
     
     def __str__(self):
+        # TODO: Fix references
         return '{id} | {created_at} | {app} | {suite} | {db_type} | {django_version}'.format(
             id=self.id, created_at=self.created_at, app=self.app,
             suite=self.suite, db_type=self.db_type, django_version=self.django_version
@@ -32,8 +34,10 @@ class Benchmark(models.Model):
 
 class BenchmarkStep(models.Model):
     benchmark = models.ForeignKey(Benchmark)
-    number = models.PositiveIntegerField()
+    step_number = models.PositiveIntegerField()
     description = models.CharField(max_length=255, blank=True, null=True)
+    ram_info_before_step = models.CharField(max_length=255, blank=True, null=True)
+    ram_info_after_step = models.CharField(max_length=255, blank=True, null=True)
     last_handled_exception = models.CharField(max_length=255, blank=True, null=True)
     last_unhandled_exception = models.CharField(max_length=255, blank=True, null=True)
     records_in = models.PositiveIntegerField(blank=True, null=True)
@@ -42,10 +46,11 @@ class BenchmarkStep(models.Model):
     end_time = models.DateTimeField(blank=True, null=True)
     duration = models.DurationField(blank=True, null=True)
     is_complete = models.NullBooleanField()
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True, blank=True, null=True)
     
     def __str__(self):
+        # TODO: Fix references
         return '{id} | Benchmark ID {bid} | Step {number} | {description}'.format(
             id=self.id, bid=self.benchmark.id,
             number=self.number, description=self.description
@@ -54,8 +59,8 @@ class BenchmarkStep(models.Model):
 class BenchmarkLog(models.Model):
     benchmark = models.ForeignKey(Benchmark)
     log = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, auto_now=True, blank=True, null=True)
     
     def __str__(self):
         return '{id} | Benchmark ID {bid} | Created at {created_at} | Updated at {updated_at}'.format(
